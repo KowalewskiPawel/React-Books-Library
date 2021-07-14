17372e76aa59dab22d621196cac60670d88d084f
Swarup Kumar Das <313swarup@gmail.com>
2021-07-15 02:54:13 +0530

    Class components are being replaced with functional components and hooks

--- src/components/List.js
+++ src/components/List.js
@@ -1,149 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-export default class List extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookAuthor: "",
-      bookName: "",
-      read: "No",
-      books: [],
-    };
-  }
+export default function List() {
+  const [bookName, setBookName] = useState('');
+  const [bookAuthor, setBookAuthor] = useState('');
+  const [read, setRead] = useState('No');
+  const [booksArr, setBooksArr] = useState([]);
+  const [readUpdate, setReadUpdate] = useState(false);
 
-  changeHandler = (event) => {
-    const nam = event.target.name;
-    const val = event.target.value;
-    this.setState({
-      [nam]: val,
-    });
-  };
-
-  submitHandler = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
-    const bookNameVal = this.state.bookName;
-    const bookAuthorVal = this.state.bookAuthor;
-    const readVal = this.state.read;
-    if (bookNameVal && bookAuthorVal) {
-      this.setState(
-        (prevState) => ({
-          books: [
-            ...prevState.books,
-            {
-              bookName: bookNameVal,
-              bookAuthor: bookAuthorVal,
-              read: readVal,
-            },
-          ],
-        }),
-        () => {
-          localStorage.setItem("books", JSON.stringify(this.state.books));
-        }
-      );
+    if (bookName && bookAuthor) {
+      const newBook = {
+        bookName,
+        bookAuthor,
+        read,
+      };
+      localStorage.setItem('books', JSON.stringify([...booksArr, newBook]));
+      setBooksArr([...booksArr, newBook]);
     }
   };
 
-  removeBook = (index) => {
-    const booksArr = [...this.state.books];
+  const removeBook = (index) => {
     if (booksArr) {
-      this.setState(
-        {
-          books: booksArr.filter((book, bookIndex) => {
-            return bookIndex !== index;
-          }),
-        },
-        () => {
-          localStorage.setItem("books", JSON.stringify(this.state.books));
-        }
-      );
+      const newArray = booksArr.filter((book, bookIndex) => {
+        return bookIndex !== index;
+      });
+
+      localStorage.setItem('books', JSON.stringify(newArray));
+      setBooksArr(newArray);
     }
   };
 
-  saveLocal = () => {
-    localStorage.setItem("books", JSON.stringify(this.state.books));
+  const saveLocal = () => {
+    localStorage.setItem('books', JSON.stringify(booksArr));
   };
 
-  componentDidMount() {
-    const books = localStorage.getItem("books");
-    if (books) this.setState({ books: JSON.parse(books) });
-  }
+  useEffect(() => {
+    const storedBooks = localStorage.getItem('books');
+    if (storedBooks) setBooksArr(JSON.parse(storedBooks));
+  }, []);
 
-  render() {
-    let books = this.state.books;
-    return (
-      <div>
-        <form className="bookForm" onSubmit={this.submitHandler}>
-          <lablel for="bookName">Book Title</lablel>
-          <input
-            id="bookName"
-            name="bookName"
-            type="text"
-            placeholder="Book Title"
-            maxLength="40"
-            onChange={this.changeHandler}
-            required
-          ></input>
-          <label for="bookAuthor">Author</label>
-          <input
-            id="bookAuthor"
-            name="bookAuthor"
-            type="text"
-            placeholder="Book Author"
-            maxLength="30"
-            onChange={this.changeHandler}
-            required
-          ></input>
-          <label>Read</label>
-          <select
-            id="read"
-            name="read"
-            onChange={this.changeHandler}
-            value={this.state.read}
-          >
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </select>
-          <input id="submit" type="submit" value="ADD NEW BOOK"></input>
-        </form>
-        <table>
+  return (
+    <div>
+      <form className="bookForm" onSubmit={(e) => submitHandler(e)}>
+        <label htmlFor="bookName">Book Title</label>
+        <input
+          id="bookName"
+          name="bookName"
+          type="text"
+          placeholder="Book Title"
+          maxLength="40"
+          onChange={(e) => setBookName(e.target.value)}
+          required
+        ></input>
+        <label htmlFor="bookAuthor">Author</label>
+        <input
+          id="bookAuthor"
+          name="bookAuthor"
+          type="text"
+          placeholder="Book Author"
+          maxLength="30"
+          onChange={(e) => setBookAuthor(e.target.value)}
+          required
+        ></input>
+        <label>Read</label>
+        <select
+          id="read"
+          name="read"
+          value={read}
+          onChange={(e) => setRead(e.target.value)}
+        >
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+        </select>
+        <input id="submit" type="submit" value="ADD NEW BOOK"></input>
+      </form>
+      <table>
+        <tbody>
           <tr>
             <th>Book Name</th>
             <th>Book Author</th>
             <th>Finished (Yes/No)</th>
             <th colSpan="2">Settings</th>
           </tr>
-          {books.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{item.bookName}</td>
-                <td>{item.bookAuthor}</td>
-                <td>{item.read}</td>
-                <td id="settings">
-                  <button
-                    onClick={() => {
-                      item.read === "Yes"
-                        ? (item.read = "No")
-                        : (item.read = "Yes");
-                      this.saveLocal();
-                      this.forceUpdate();
-                    }}
-                  >
-                    {item.read === "Yes" ? "Still reading" : "Finished"}
-                  </button>
+          {booksArr
+            ? booksArr.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.bookName}</td>
+                    <td>{item.bookAuthor}</td>
+                    <td>{item.read}</td>
+                    <td id="settings">
+                      <button
+                        onClick={() => {
+                          item.read === 'Yes'
+                            ? (item.read = 'No')
+                            : (item.read = 'Yes');
+                          saveLocal();
+                          setReadUpdate(!readUpdate);
+                        }}
+                      >
+                        {item.read === 'Yes' ? 'Still reading' : 'Finished'}
+                      </button>
 
-                  <button
-                    onClick={() => {
-                      this.removeBook(index);
-                    }}
-                  >
-                    Remove
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </table>
-      </div>
-    );
-  }
+                      <button onClick={() => removeBook(index)}>Remove</button>
+                    </td>
+                  </tr>
+                );
+              })
+            : null}
+        </tbody>
+      </table>
+    </div>
+  );
 }
